5e51bc6ed15cc369000e0065a0014fb1f12983d9
aimee-liang <aimeeeliang@gmail.com>
2022-05-03 11:19:42 -0400

    Included convertToString, isPalindrome, isPangram, reverseString

--- strings.js
+++ strings.js
@@ -1,11 +1,34 @@
 const strings = {}
 
-strings.IsString = (arg) => {
+strings.IsString = arg => {
     return (typeof arg === 'string')
 }
 
-strings.stringLength = (str) => {
+strings.stringLength = str => {
     return str.length
+}
+
+strings.convertToString = arg => {
+    return arg.toString()
+}
+
+strings.isPalindrome = str => {
+    let newStr = str.split('').reverse().join('')
+    return (str === newStr)
+}
+
+strings.isPangram = str => {
+    let newStr = ([...new Set(str)]).join('')
+    if (newStr.length === 26){
+        return true
+    } else {
+        return false
+    }
+}
+
+strings.reverseString = str => {
+    str = str.split('').reverse().join('')
+    return str
 }
 
 module.exports = strings
--- tests/strings.test.js
+++ tests/strings.test.js
@@ -1,3 +1,5 @@
+// const { expect } = require('chai')
+// const expect = require('chai/lib/chai/interface/expect')
 const strings = require('../strings')
 
 test('Number passed in as a arg will not be evaluated as a string', () => {
@@ -10,4 +12,24 @@ test('Empty string should return true as string', () => {
 
 test('Length of string `qwertyuiopasdfghjklzxcvbnm` to be 26', () => {
     expect(strings.stringLength('qwertyuiopasdfghjklzxcvbnm')).toBe(26)
+})
+
+test('Argument should be converted as a string', () => {
+    expect(strings.convertToString(1502)).toBe('1502')
+})
+
+test('Return true if string is a palindrome', () => {
+    expect(strings.isPalindrome('racecar')).toBe(true)
+})
+
+test('Return false if string is not a palindrome', () => {
+    expect(strings.isPalindrome('pizza')).toBe(false)
+})
+
+test('Return true if pangram, false if otherwise', () => {
+    expect(strings.isPangram('qwertyuioppppplkjhgfdsamnbvcxzazzz')).toBe(true)
+})
+
+test('Return reversed string', () => {
+    expect(strings.reverseString('abc')).toBe('cba')
 })
